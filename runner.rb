8b0dbcbaa3d0424a21212fb754f497fa03033a75
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2016-04-12 11:02:02 -0600

    unpack gems

--- runner.rb
+++ runner.rb
@@ -24,6 +24,18 @@ def copy_gems_into_subdirectories
   end
 end
 
-create_directories
-copy_gems_into_subdirectories
+def unpack_gems
+  1.upto(buckets) do |num|
+    dir = "gem_subset_#{num}"
+    `ls #{dir}`.strip.split(/\s+/).each do |gem|
+      cmd = "gem unpack #{dir}/#{gem} --target=#{dir}"
+      puts cmd
+      `#{cmd}`
+    end
+  end
+end
+
+# create_directories
+# copy_gems_into_subdirectories
+unpack_gems
 
