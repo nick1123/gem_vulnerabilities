fc2012b1ecbc11dfa1e5f9202d704c8e850ac39a
Stewie (Nicholas Stewart) <nstewart@instructure.com>
2016-04-13 11:24:17 -0600

    add file for ip address searching

--- search_for_ip_addresses.rb
+++ search_for_ip_addresses.rb
@@ -4,14 +4,6 @@ def find_command(directory)
   "find #{directory} -name '*.rb' | grep -v \"/spec/\\|/test/\""
 end
 
-def search_for_net_http_command(directory)
-  "#{find_command(directory)} | xargs grep -n Net::HTTP"
-end
-
-def search_for_http_url_command(directory)
-  "#{find_command(directory)} | xargs grep -ni \"http://\""
-end
-
 def search_for_ip_address_command(directory)
   "#{find_command(directory)} | xargs grep -n '[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}'"
 end
@@ -20,18 +12,22 @@ def execute(command)
   `#{command}`
 end
 
-def look_for_urls(directory)
-  execute(search_for_http_url_command(directory))
-end
-
 def remove_undesirable_lines(command_results)
   break_into_lines(command_results).map { |line|
+    # Example line:
+    # gem_subset_1/Sipper-1.1.3/sipper/transaction/transaction.rb:10: Td = 32000 # 17.1.1.2
     line.split(/\s+/)  # break each line into pieces
   }.reject { |line_pieces|
+    # Example line to reject:
+    # gem_subset_1/Sipper-1.1.3/sipper/transaction/transaction.rb:10: # Td = 32000 && 17.1.1.2
     line_pieces.size < 2 || line_pieces[1][0] == '#' # reject line comments
   }.reject { |line_pieces|
+    # Example line to reject:
+    # gem_subset_1/Sipper-1.1.3/sipper/transaction/transaction.rb:10:# Td = 32000 && 17.1.1.2
     line_pieces[0][-1] == '#' # reject line comments
   }.select { |line_pieces|
+    # Example line to reject:
+    # gem_subset_1/Sipper-1.1.3/sipper/transaction/transaction.rb:10: Td = 32000 # 17.1.1.2
     line_pieces.any? {|piece| piece =~ Resolv::IPv4::Regex}
   }.map { |line_pieces|
     line_pieces.join(' ')
@@ -55,10 +51,10 @@ def break_into_lines(command_results)
   command_results.strip.split("\n").map {|line| line.strip }
 end
 
-# puts execute(search_for_net_http_command('gem_subset_1'))
-# puts look_for_urls('gem_subset_1')
+last = 10
+last = ARGV[0].to_i if !ARGV[0].nil?
 
-puts (1..2).inject([]) { |results, dir_num|
+puts (1..last).inject([]) { |results, dir_num|
   results.concat(
     remove_undesirable_lines(
       execute(
